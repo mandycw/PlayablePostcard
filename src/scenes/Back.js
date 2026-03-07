03307efb5bce5c19aba31c7c5cdbe2d66f30508b
Mandy Cai <mandyycai@gmail.com>
2026-03-06 22:21:23 -0800

    made front scene more efficent

--- src/scenes/Back.js
+++ src/scenes/Back.js
@@ -6,16 +6,32 @@ class Back extends Phaser.Scene{
     preload(){
         this.load.path = "./assets/"
         this.load.image('postcard', 'postcard.png')
+        this.load.atlas('sheet', 'japan.png', 'japan.json')
+        this.load.json('shapes', 'japn.json')
     }
 
     create(){
         this.postcard = this.add.image(0, 0, 'postcard').setOrigin(0)
         this.postcard.setDisplaySize(800, 600)
+
+        var shapes = this.cache.json.get('shapes')
+        this.matter.world.setBounds(0, 0, game.config.width, game.config.height)
+
+        var mask = this.matter.add.sprite(0, 0, 'sheet', 'mask.png', {shape: shapes.mask})
+        mask.setPosition(100, 100)
+        mask.setDisplaySize(100, 130)
+        this.matter.world.remove(mask)
+
+        this.input.keyboard.on('keydown-F', () => {
+        this.scene.start('frontScene')
+        })
     }
 
     update(){
-        this.input.keyboard.on('keydown-F', ()=>{
-            this.scene.start('frontScene')
-        })
+        
+    }
+
+    sticker(){
+        //handle drag events for sprites 
     }
 }
--- src/scenes/Front.js
+++ src/scenes/Front.js
@@ -12,51 +12,43 @@ class Front extends Phaser.Scene{
         this.sky = this.add.image(0, 0, 'sky').setOrigin(0)
         this.sky.setDisplaySize(800, 600)
         
-        // this.sun = this.add.image(300, 170, 'sun').setOrigin(0)
-        // this.sun.setDisplaySize(100, 100)
-        // this.fuji = this.add.image(0, 75, 'mtfuji').setOrigin(0)
-        // this.fuji.setDisplaySize(800, 600)
-        
-        // this.tori = this.add.image(150, 400, 'tori')
-        // this.tori.setDisplaySize(300, 300)
-
-        // 
-        
-        
-        var shapes = this.cache.json.get('shapes')
+        //var shapes = this.cache.json.get('shapes')
         this.matter.world.setBounds(0, 0, game.config.width, game.config.height)
-        var mtfuji = this.matter.add.sprite(0, 0, 'sheet', 'mtfuji.png', {shape: shapes.mtfuji})
-        mtfuji.setPosition(400, 400)
-        mtfuji.setDisplaySize(800, 600)
-        var tori = this.matter.add.sprite(0, 0, 'sheet', 'torigate.png', {shape: shapes.torigate})
-        tori.setPosition(200, 400)
-        tori.setDisplaySize(300, 300)
-        this.matter.world.remove(tori)
-        this.matter.world.remove(mtfuji)
+
+        var sun = this.add.sprite(350, 200, 'sheet', 'sun.png');
+        sun.setDisplaySize(100, 100);
+        sun.setPosition(350, 200)
+
         this.cherry = this.add.image(100, -75, 'cherry').setOrigin(0)
         this.cherry.setDisplaySize(700, 500)
 
-        tori.setInteractive({ pixelPerfect: true })
-        tori.on('pointerdown', function (pointer, localX, localY, event) {
-        console.log('torigates are cool') })
+        this.interactableItem(350, 200, 'sun.png', 100, 100, 'the sun is a deadly laser');
+        this.interactableItem(400, 400, 'mtfuji.png', 800, 600, 'mt fuji is a mountain');
+        this.interactableItem(200, 400, 'torigate.png', 300, 300, 'torigates are cool')
 
-        mtfuji.setInteractive({ pixelPerfect: true })
-        mtfuji.on('pointerdown', function (pointer, localX, localY, event) {
-        console.log('mt fuji is a mountain') })
+        this.input.keyboard.on('keydown-F', () => {
+        this.scene.start('backScene')
+    })
         
     }
     update(){
 
-        this.input.keyboard.on('keydown-F', ()=>{
-            this.scene.start('backScene')
-        })
+        
     }   
 
 
-    interactableItem(){
+    interactableItem(x, y, name, width, height, message){
         //glow
         //listener for pointerdown
         //
+        let item = this.add.sprite(x, y, 'sheet', name)
+        item.setDisplaySize(width, height)
+        item.setInteractive({ pixelPerfect: true })
+        item.on('pointerdown', ()=> {
+            console.log(message)
+            //popup()
+        })
+        return item
     }
 
     popUp(){
